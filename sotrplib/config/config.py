9ef5f14b1122637dd2b145bad5c4edc451812290
Mike Peel <email@mikepeel.net>
2025-07-03 19:04:25 +0100

    expand

--- sotrplib/config/config.py
+++ sotrplib/config/config.py
@@ -1,16 +1,30 @@
 from typing import Any
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import BaseModel, Field, ValidationError, AliasChoices
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 from pathlib import Path
 import json
 
 class Settings(BaseSettings):
-	test: int = 1
-	teststr: str = 'Test'
+	# Telescope choice
+	observatory: str = "SO"
+	instrument: str = "LAT"
 
-	model_config = SettingsConfigDict(env_prefix='sotrp_')
+	# File locations
+	maps: str = "" # input maps... the map.fits ones.
+	map_dir: str = "" # Location of the depth-1 maps
+	solar_system: str = "" # path asteroid ephem directories	
+	output_dir: str = "" # path to output databases and / or plots
+	source_cat: str = "" # path to source catalog
 
+	# Map settings
+	grid_size: float = Field(default=1.0, validation_alias=AliasChoices('g','grid-size', 'gridsize')) # Flatfield tile gridsize (deg)
+	edge_cut: int = 20 # Npixels to cut from map edges
+
+	# Read environment and command line settings to override default
+	model_config = SettingsConfigDict(env_prefix='sotrp_', cli_parse_args=True, extra='ignore')
+
+	# Read json config settings to override default + environment
 	@classmethod
 	def from_file(cls, config_path: Path | str) -> "Settings":
 		with open(config_path, "r") as handle:
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,13 @@
 from sotrplib.config.config import Settings
 
 # These are the default settings
-print(Settings)
 print(Settings().model_dump())
 
 # This is reading in from a file
 MySettings = Settings.from_file("/Users/mpeel/Documents/git/sotrplib/sotrplib/config/test_config.json")
 
 print(MySettings)
-print(MySettings.test)
+
+MySettings = Settings.from_file("/Users/mpeel/Documents/git/sotrplib/sotrplib/config/tiger3_config.json")
+
+print(MySettings)
